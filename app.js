b809a3aaf1db0cd97a0502ae86bcfeae6a026796
Tony Chao <tchao@mail.usf.edu>
2022-03-18 17:27:10 -0400

    Render posts from app's database to the home page

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -19,7 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/dailyJournalDB");
 
-const postsSchema = mongoose.Schema({
+const postsSchema = new mongoose.Schema({
   title: {
     type: String,
     require: true
@@ -33,9 +31,11 @@ const postsSchema = mongoose.Schema({
 const Post = mongoose.model("Post", postsSchema);
 
 app.get("/", function(req, res) {
-  res.render("home", {
-    homeContent: homeStartingContent,
-    posts: posts
+  Post.find({}, function(error, posts) {
+    res.render("home", {
+      homeContent: homeStartingContent,
+      posts: posts
+    });
   });
 });
 
@@ -58,7 +58,7 @@ app.get("/compose", function(req, res) {
 app.post("/compose", function(req, res) {
   const post = new Post({
     title: req.body.postTitle,
-    content: req.body.content
+    content: req.body.postBody
   });
 
   post.save(function(error) {
